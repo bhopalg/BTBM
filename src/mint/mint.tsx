3a0e6820088ad70ee5148be8f6ca571262ae38fb
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-05-30 18:01:03 +0100

    reworked minting page

--- src/mint/mint.tsx
+++ src/mint/mint.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './mint.css';
 import { useMediaQuery } from 'react-responsive';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import Section1Image from '../assets/images/homepage-section-1.png';
 // import { ethers } from 'ethers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,6 +9,10 @@ import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Mint() {
   const isMobile = useMediaQuery({ query: '(max-width: 576px)' });
+  const isTablet = useMediaQuery({
+    query: '(min-width: 576px) and (max-width: 1224px)',
+  });
+
   const [mintAmount, setMintAmount] = useState(1);
 
   function incrementMintAmount() {
@@ -26,6 +30,87 @@ function Mint() {
 
     setMintAmount(mintAmount - 1);
   }
+
+  let containerClass = '';
+  if (isMobile) {
+    containerClass = 'mobile-mint-container mint-container';
+  } else if (isTablet) {
+    containerClass = 'tablet-mint-container mint-container';
+  } else {
+    containerClass = 'mint-container';
+  }
+
+
+  return (
+    <Container
+      className={containerClass}>
+      <Row>
+        <Col sm={12} md={5}>
+          <img
+            alt="Section 1"
+            src={Section1Image}
+            className="d-inline-block section-one-image"
+          />
+        </Col>
+        <Col sm={12} md={3} className={'mint-section'}>
+          <Row>
+            <Col xs={12}>
+              <Row className={'mint-input-section'}>
+                <Col
+                  xs={2}
+                  className={'mint-input-arrows mint-input-arrow-left'}
+                >
+                  <button
+                    className={'mint-amount-buttons'}
+                    onClick={decrementMintAmount}
+                    disabled={mintAmount === 1}
+                  >
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                  </button>
+                </Col>
+                <Col xs={6}>
+                  <Form.Control
+                    type="number"
+                    min="0"
+                    max="3"
+                    aria-describedby="amount"
+                    value={mintAmount}
+                  />
+                </Col>
+                <Col
+                  xs={2}
+                  className={'mint-input-arrows mint-input-arrow-right'}
+                >
+                  <button
+                    className={'mint-amount-buttons'}
+                    onClick={incrementMintAmount}
+                    disabled={mintAmount === 3}
+                  >
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </button>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12}>
+              <Row>
+                <Col>
+                  <Button
+                    disabled={true}
+                    className={'mint-button'}
+                    variant="outline-dark"
+                    href={'/mint'}
+                  >
+                    MINT
+                  </Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+
 
   return (
     <div
