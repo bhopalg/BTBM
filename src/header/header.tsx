42057a7e976d5e529a54663d345983ff229395b1
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-06-05 14:28:00 +0100

    link logo to whitepaper

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -93,7 +93,8 @@ function Header(props: Props) {
                     <a
                       rel={'noreferrer'}
                       className={'logo-container'}
-                      href={'/'}
+                      href={'https://meenft.notion.site/MEE-Whitepaper-c29eee9dde0541e38cbb6bd9a4798050'}
+                      target={'_blank'}
                     >
                       <img
                         alt="Logo"
@@ -147,8 +148,9 @@ function Header(props: Props) {
   const mobileNav = (
     <Navbar bg="light" expand="lg" className={'mobile-navbar'}>
       <Container>
-        <Navbar.Brand href="#home" className={'logo-container'}>
-          <a href={'/'} rel={'noreferrer'} className={'logo-name'}>
+        <Navbar.Brand className={'logo-container'}>
+          <a href={'https://meenft.notion.site/MEE-Whitepaper-c29eee9dde0541e38cbb6bd9a4798050'}
+               target={'_blank'} rel={'noreferrer'} className={'logo-name'}>
             BORN TO BE ME
           </a>
         </Navbar.Brand>
